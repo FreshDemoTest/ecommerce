0bda1be1f2ffe43bded5de1ca0aee66b3a751842
FernandoEdwinReyesReyes <fernandoreyesreyes1@gmail.com>
2024-10-09 11:15:59 -0600

    fix: log env vars

--- apps/commerce-template/app/config.ts
+++ apps/commerce-template/app/config.ts
@@ -1,5 +1,6 @@
 import type { EnvCommerceConfigType } from "core";
 
+console.log(process.env)
 const graphQLAPIUrl: string =
   process.env.NEXT_PUBLIC_GQLAPI_ENV === "production"
     ? "https://gql-standalone.onrender.com/graphql"
